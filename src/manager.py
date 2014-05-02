25577107d82e775cc6c6338ef3350c70bb1173db
sch0414 <skywind143@naver.com>
2014-05-02 15:18:04 +0900

    message

--- src/manager.py
+++ src/manager.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
+from kazoo.clinet import KazooClient
 
 //Init - ZK connection, node watch
+zk=KazooClient(hosts='127.0.0.1:2181') //default port
+zk.start();
 
+@zk.ChildrenWatch("gateways")
+def watch_gw_children(children)
+	print("Childrent are now: %s" % children)
+# Above function called immediately, and from then on
 
+@zk.DataWatch("gateways")
+def watch_node(data,stat):
+	print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
 //main loop - ZK Events Management - new ephemeral node,
 //				ephemeral node change, scale in/out
 
