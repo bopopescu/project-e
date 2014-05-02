001cb1f562868f18402841b6b57c815c6609e501
byeongchang.kim@gmail.com <kbc@ubuntu.ubuntu-domain>
2014-05-02 16:39:12 +0900

    cellserver

--- src/cellserver.py
+++ src/cellserver.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
+import socket
+import sys
+import pika
 
-#Init : MQ connection, creation of world(Oh...)
-    
+##########Init : MQ connection, creation of world(Oh...)#############3
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 8000)
+print
+
+
 #loop : receive c-move and correct data according to c-move
 #       broad casting - s_put, s_move, s_del - list division
 
-import SimpleHTTPServer
-import SocketServer
 
 PORT = 8000
 
