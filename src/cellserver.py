f8b0ecdcf86b971397e41f8509a27b6f45755a5c
byeongchang.kim@gmail.com <kbc@ubuntu.ubuntu-domain>
2014-05-02 15:02:19 +0900

    cellserver

--- src/cellserver.py
+++ src/cellserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-//Init : MQ connection, creation of world(Oh...)
-
-//loop : receive c-move and correct data according to c-move
-//       broad casting - s_put, s_move, s_del - list division
+#Init : MQ connection, creation of world(Oh...)
+    
+#loop : receive c-move and correct data according to c-move
+#       broad casting - s_put, s_move, s_del - list division
