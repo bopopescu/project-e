9e497ddcacd18223085690079b911cba9f410616
byeongchang.kim@gmail.com <kbc@ubuntu.ubuntu-domain>
2014-05-02 15:41:21 +0900

    cellserver

--- src/cellserver.py
+++ src/cellserver.py
@@ -4,3 +4,15 @@
     
 #loop : receive c-move and correct data according to c-move
 #       broad casting - s_put, s_move, s_del - list division
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
