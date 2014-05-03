79c0de54c5d3e87dcc16b9c756ac8325f331ee15
tarquis <jeongyoon0807@gmail.com>
2014-05-03 16:03:43 +0900

    client

--- src/client.py
+++ src/client.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python
+
+  import zmq
+  import time
+
+  context = zmq.Context()
+
+  # Socket
+  socket = context.socket(zmq.REQ)
+  socket.connect("tcp://localhost:5555")
+
+  # Send request
+  while True:
+    print("Sending request %s" % request)
+    socket.send("Request")
+    # Get reply
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message))
+    time.sleep(10)
