a82007321b8f187842496d4e24c329db508f1c73
byeongchang.kim@gmail.com <kbc@ubuntu.ubuntu-domain>
2014-05-03 13:11:27 +0900

    cellserver

--- src/cellserver.py
+++ src/cellserver.py
@@ -5,18 +5,7 @@ import pika
 import zmq
 import time
 
-######
-#TCP_IP = '127.0.0.1'
-#TCP_PORT = 8000
-#BUFFER_SIZE = 1024
-#
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # request socket from the kernel
-#s.bind((TCP_IP, TCP_PORT)) # set socket address
-#s.listen(1) # socket is waiting for connection
-#
-#conn, addr = s.accept() # approve connection
-#print 'Connection address:', addr
-######
+#make server by using zmq.REP and zmp.REQ
 
 #Echo server example
 context = zmq.Context()
@@ -56,10 +45,3 @@ while True:
 # Receive c_move and correct data according to c_move
 # Broadcasting - s_put, s_move, s_del - list division
 ########################
-
-
-#while 1:
-#    data = conn.recv(BUFFER_SIZE)
-#    if not data: break
-#    conn.send(data) # echo
-#conn.close()
