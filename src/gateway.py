237925c224d7b168fc33be402f53d10c2d4ee3c6
sch0414 <skywind143@naver.com>
2014-05-03 16:23:30 +0900

    gateway with kazoo

--- src/gateway.py
+++ src/gateway.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
 import zmq
+from kazoo.client import KazooClient
+from kazoo.client import KazooState
 #Init : Protocol Handler, MQ Connection, ZK Connection
+#MQ Connection
 context=zmq.Context();
 connect_cnt=0;
 socket_client=context.socket(zmq.REP);
 socket_cell=context.socket(zmq.REQ);
+
+#connection to zookeeper
+zk=KazooClient(hosts='127.0.0.1:2181');
+zk.start();
+def my_listener(state):
+{	
+	if state==KazooState.LOST:
+		zk.delete("gateways/node",recursive=True);
+	#Register somewhere that the session was lost
+	elif state==KazooState.SUSPENDED:
+		zk.delete("/gateways/node",recursive=True);
+	#Handle being disconnected from Zookeeper
+	else:{
+		zk.ensure_path("/gateways");
+		zk.create("/gateways/node", connect_cnt);
+	}#Handle being connected/reconnected to Zookeeper
+}
+zk.add_listener(my_listener);
+
+
+
+
 
 #temporary tcp location
 socket_client.bind("tcp://127.0.0.1:5000");
