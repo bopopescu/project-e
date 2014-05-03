8d885ad581a3582bf977022f159ffe45702e7187
byeongchang.kim@gmail.com <kbc@ubuntu.ubuntu-domain>
2014-05-03 16:49:48 +0900

    cellserver

--- src/cellserver.py
+++ src/cellserver.py
@@ -10,12 +10,10 @@ socket_cell = context.socket(zmq.REP)
 client_list = []
 
 #Define c_move handler
-def cmove_handler ():
+def cmove_handler (message):
+    #save client list
+    #Broadcasting - s_put, s_move, s_del - list division
 
-#Define s_put, s_move, s_del
-s_put = "sput"
-s_move = "smove"
-s_del = "sdel"
 
 #temporary tcp location
 socket_cell.bind("tcp://127.0.0.1:5000")
@@ -25,14 +23,10 @@ socket_cell.bind("tcp://127.0.0.1:5000")
 
 
 ##########Loop##########
-# Receive c_move and correct data according to c_move
 while True:
-    message = socket_cell.recv()
-    if (msg = "cmove")
+    msg = socket_cell.recv()
+    #Receive c_move
+    if msg[0:4] = "cmove":
         print "Receive", msg
-
-    #Broadcasting - s_put, s_move, s_del - list division
-    socket_cell.send(s_put)
-    socket_cell.send(s_move)
-    socket_cell.send(s_del)
-
+        #save data and broadcasting
+        cmove_handler(msg)
