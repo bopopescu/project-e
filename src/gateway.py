d240f57b8c652812538b50b21864511ba64a1b95
sch0414 <skywind143@naver.com>
2014-05-03 15:04:10 +0900

    gateway revision

--- src/gateway.py
+++ src/gateway.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
+import zmq
+#Init : Protocol Handler, MQ Connection, ZK Connection
+context=zmq.Context();
+connect_cnt=0;
+socket_client=context.socket(zmq.REP);
+socket_cell=context.socket(zmq.REQ);
 
-//Init : Protocol Handler, MQ Connection, ZK Connection
+#temporary tcp location
+socket_client.bind("tcp://127.0.0.1:5000");
+socket_cell.connect("tcp://127.0.0.1:2492");
 
-//loop1 : Connection Accept, receive messages from clients -> send to MQ
-
-//loop2 : receive messages from MQ -> sen to clients
-
-//loop3 : If clients cut connection, then decrease client count
-
+#loop1 : Connection Accept, receive messages from clients -> send to MQ
+#while True...?True...? I don't know. @_@
+#
+while True:
+{
+	msg=socket_client.recv()
+	if (msg="connected"):#connect count++
+	connect_cnt++
+	elif (msg="cut"):#connect count--
+	connect_cnt--
+	elif (str.startswith('cmove'))
+	socket_cell.send(msg)
+}	
+#loop2 : receive messages from MQ -> sen to clients
+while True:
+{
+	msg_cell=socket_cell.recv()
+#Maybe we need to filter msg_cell. But how?...
+	socket_client.send(msg_cell)
+}
